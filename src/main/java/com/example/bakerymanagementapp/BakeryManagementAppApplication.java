f6b5a3edbf972030e9b82961c6d20f5eacd478c8
Eric Ngako <ericnk1@live.com>
2021-03-03 20:54:10 +0200

    initial commit

---
+++ src/main/java/com/example/bakerymanagementapp/BakeryManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bakerymanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryManagementAppApplication.class, args);
+    }
+
+}
