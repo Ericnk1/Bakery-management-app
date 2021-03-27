418a691a5bdf83c2f5a83e5a1b3ebff5643cdbe0
Eric Ngako <ericnk1@live.com>
2021-03-27 23:33:34 +0200

    updated

--- src/main/java/com/example/bakerymanagementapp/model/Employee.java
+++ src/main/java/com/example/bakerymanagementapp/model/Employee.java
@@ -4,15 +4,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Employee {
+public class Employee implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private long id;
 
     private String name;
@@ -20,4 +22,6 @@ public class Employee {
     private String email;
 
     private boolean isActive;
+    private String jobTitle;
+    private String imageUrl;
 }
